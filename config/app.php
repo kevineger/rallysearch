d90e352b851777b6ba9a3dd207acb63edaf26fa9
kevineger <egerkevinjames@gmail.com>
2016-03-31 17:02:00 -0700

    Includes application depedencies for Google Cloud Services and the convenience of having laravel-ide-helper (for developing in phpStorm).

--- config/app.php
+++ config/app.php
@@ -156,6 +156,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Application Specific Service Providers
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -201,6 +207,12 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+         * Application specific aliases.
+         */
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
 
     ],
 
