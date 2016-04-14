1da923b2d0c7f998ecaef8ee172b52fcf140df0c
kevineger <egerkevinjames@gmail.com>
2016-04-14 15:38:31 -0700

    Pushes annotating task to hourly.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,6 @@ class Kernel extends ConsoleKernel {
      * @var array
      */
     protected $commands = [
-        // Commands\Inspire::class,
         Commands\AnnotateTop::class,
     ];
 
@@ -28,10 +27,6 @@ class Kernel extends ConsoleKernel {
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('annotate:top')->twiceDaily();
-//        $schedule->call(function ()
-//        {
-//            error_log("Hitting cloud url");
-//        })->everyMinute()->thenPing(url('cloud'));
+        $schedule->command('annotate:top')->hourly();
     }
 }
